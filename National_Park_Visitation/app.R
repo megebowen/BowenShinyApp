3bc9ce718852bcc34050b95ddc0dc072cb242615
Meghan Bowen <mebowen14@gmail.com>
2019-02-28 15:30:49 -0800

    more updates to shiny whoo hoo

--- National_Park_Visitation/app.R
+++ National_Park_Visitation/app.R
@@ -28,7 +28,7 @@ ui <- fluidPage(
                            ,
                            
                            br(),
-                           
+
                            radioButtons("radio", label = h3("Month"),
                                         choices = list("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5, "June" = 6, "July" = 7, "August" = 8, "September" = 9, "October" = 10, "November" = 11, "December" = 12), 
                                         selected = 1),
